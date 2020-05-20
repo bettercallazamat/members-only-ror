5a5ec5c9c1fdb3caee1e2df26bc75269b65c2245
bettercallazamat <bettercallazamat@gmail.com>
2020-05-21 02:30:48 +0600

    Add navigation for application layout and index for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,14 +22,6 @@ class PostsController < ApplicationController
     end
   end
 
-  private
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def edit
     @post = Post.find(params[:id])
   end
@@ -37,7 +29,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to posts_path
+      redirect_to root_path
     else
       render edit_post_path(params[:id])
     end
@@ -46,6 +38,15 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
+    redirect_to root_path
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+
 end
