878886038d9d489a3a2fad122c192a3ada08d271
bettercallazamat <bettercallazamat@gmail.com>
2020-05-21 19:57:35 +0600

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :posts
+  resources :posts, :except => [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "posts#index"
 end
